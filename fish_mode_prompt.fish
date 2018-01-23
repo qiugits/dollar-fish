a60152b6a968d54b771991169f5213b43215782e
Qiu <qiu.gits@gmail.com>
2018-01-23 17:27:53 +0900

    Change to Dollar

--- fish_mode_prompt.fish
+++ fish_mode_prompt.fish
@@ -1,6 +1,6 @@
-# This is handled inside fish_mode_prompt (see `__bobthefish_prompt_vi`)
+# This is handled inside fish_mode_prompt (see `__dollar_prompt_vi`)
 #
-# If you want to override this with your own mode prompt, disable bobthefish's
+# If you want to override this with your own mode prompt, disable dollar's
 # built-in mode prompt in your fish config:
 #
 #     set -g theme_display_vi no
